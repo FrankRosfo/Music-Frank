a3ceb7c70b099a9e7180e74f106f2fd9d5cbe22b
VibeFJ <90203526+FrankRosfo@users.noreply.github.com>
2022-03-30 03:46:41 -0600

    Funcionando??

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,7 +37,7 @@ return [
         ],
 
         'public' => [
-            'driver' => 'local',
+            'driver' => 'public',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
